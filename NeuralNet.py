7334d9ce54c50db854c4839a1c9f5884c394b631
HOLYPERSON <31584910+omy181@users.noreply.github.com>
2023-11-25 20:29:49 +0300

    Initial

---
+++ NeuralNet.py
@@ -0,0 +1,6 @@
+import pandas as pandas
+import numpy as numpy
+
+
+def TrainNeuralNetwork(pandas_data):
+    return
---
+++ Test.py
@@ -0,0 +1,7 @@
+import pandas as pandas
+import NeuralNet
+
+
+Pandas_Data = pandas.read_csv("midtermProject-part1-TRAIN.csv")
+
+TrainNeuralNetwork(Pandas_Data)

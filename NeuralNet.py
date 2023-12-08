006b9a8efed7198abadfaf34e000272d2aad03eb
HOLYPERSON <31584910+omy181@users.noreply.github.com>
2023-12-08 23:16:45 +0300

    dynamic layers

--- NeuralNet.py
+++ NeuralNet.py
@@ -26,27 +26,21 @@ class Neural_Network(nn.Module):
     def __init__ (self,I,layers):
         super().__init__()
         
-        self.l1 = nn.Linear(I,layers[0])
-        self.l2 = nn.Linear(layers[0],layers[1])
-
-        """self.layerslist = []
+        self.layerslist = nn.ModuleList()
 
         self.layerslist.append(nn.Linear(I,layers[0]))
         for layerindex,_ in enumerate(layers,1):   
             if layerindex == len(layers): break         
-            self.layerslist.append(nn.Linear(layers[layerindex-1],layers[layerindex]))"""
+            self.layerslist.append(nn.Linear(layers[layerindex-1],layers[layerindex]))
 
 
     def forward_propogation(self,x):
 
-        x = f.relu(self.l1(x))
-        x = self.l2(x)
-
-        """ for layerindex,layer in enumerate(layerslist): 
-            if layerindex == len(layerslist)-1: break   
-            x = f.relu(self.layer(x))
+        for layerindex,layer in enumerate(self.layerslist): 
+            if layerindex == len(self.layerslist)-1: break   
+            x = f.relu(layer(x))
         
-        x = self.layerslist[-1](x)"""
+        x = self.layerslist[-1](x)
         return x
 
 

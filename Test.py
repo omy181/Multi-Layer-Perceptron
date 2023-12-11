7e14e0e16af012643e86614ac04fb423e74b637b
HOLYPERSON <31584910+omy181@users.noreply.github.com>
2023-12-11 17:24:45 +0300

    Documentations

--- Test.py
+++ Test.py
@@ -7,3 +7,8 @@ TestData = read_csv("midtermProject-part1-TEST.csv")
 model = TrainNeuralNetwork(TrainData,[3,1],TruthLabel="ANGLE-ACC-ARM",learning_rate=0.01,epoch=1000)
 
 TestNeuralNetwork(model,TestData,TruthLabel="ANGLE-ACC-ARM")
+
+# printe weights and biases
+for name,val in model.named_parameters():
+    print("Node:",name)
+    print("Values:",val)
